5c4492017ef8f49fdb27ed14e131381dc94f3c87
Qaqambile Makana <solarcouple@gmail.com>
2021-04-15 08:10:53 +0200

    makana onlineshop

---
+++ makanaonline/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def store(request):
+    context ={ }
+    return render(request, 'store.html', {'context':context })
+
+
+def cart(request):
+    context ={ }
+    return render(request, 'cart.html', {'context':context })
+
+def checkout(request):
+    context ={ }
+    return render(request, 'checkout.html', {'context':context })
